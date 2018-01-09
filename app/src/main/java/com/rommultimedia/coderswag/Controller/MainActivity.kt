a3ff59077b079051874b38c367a2d2a66a91da32
JRICARDOROMERO <info@rommultimedia.com>
2018-01-08 22:56:44 -0500

    Second commit

--- app/src/main/java/com/rommultimedia/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/rommultimedia/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,24 @@ package com.rommultimedia.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.rommultimedia.coderswag.Model.Category
 import com.rommultimedia.coderswag.R
+import com.rommultimedia.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+
+        CategoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/rommultimedia/coderswag/Model/Category.kt
+++ app/src/main/java/com/rommultimedia/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.rommultimedia.coderswag.Model
 /**
  * Created by User on 1/8/2018.
  */
-class Category (val title:String, val image:String)
+class Category (val title:String, val image:String){
+    override fun toString(): String {
+        return title
+    }
+}
